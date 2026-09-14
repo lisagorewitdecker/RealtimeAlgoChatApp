df857252c28b2d72983e226b52784cf2a77dcd58
lisagorewitdecker <50784106+lisagorewitdecker@users.noreply.github.com>
2026-09-14 17:33:10 -0400

    Add files via upload
    Signed-off-by: lisagorewitdecker <50784106+lisagorewitdecker@users.noreply.github.com>

--- lib/api-client-react/src/index.ts
+++ lib/api-client-react/src/index.ts
@@ -1,1 +1,4 @@
-export {};
+export * from "./generated/api";
+export * from "./generated/api.schemas";
+export { setBaseUrl, setAuthTokenGetter } from "./custom-fetch";
+export type { AuthTokenGetter } from "./custom-fetch";
